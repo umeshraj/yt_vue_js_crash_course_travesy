8c2f2d998366844678c6745ce816cbc5e7d81c9d
Umesh Rajashekar <umesh.rajashekar@gmail.com>
2020-06-30 08:30:25 -0400

    Looping over todo items

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,37 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <Todos v-bind:todos="todos" />
+  </div>
 </template>
 
 <script>
+import Todos from "./components/Todos";
 export default {
   name: "App",
-  components: {}
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    };
+  }
 };
 </script>
 
